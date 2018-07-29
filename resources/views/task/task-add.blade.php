667018a30e8e684f6a982399e541ca75324f5285
Amitav Roy <reachme@amitavroy.com>
2018-07-29 09:21:48 +0530

    updated the layout of Task add based on the new theme

--- resources/views/task/task-add.blade.php
+++ resources/views/task/task-add.blade.php
@@ -1,25 +1,41 @@
 @extends('layouts.app')
 
+@section('breadcrumb')
+    <div class="app-title">
+        <div>
+            <h1><i class="fa fa-dashboard"></i> Add Task</h1>
+            <p>Add new task to my list.</p>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="{{route('home')}}">
+                    <i class="fa fa-home fa-lg"></i>
+                </a>
+            </li>
+            <li class="breadcrumb-item">
+                <a href="{{route('task.index')}}">Tasks</a>
+            </li>
+            <li class="breadcrumb-item">Add tasks</li>
+        </ul>
+    </div>
+@endsection
+
 @section('content')
-    <div class="container">
-        <div class="row justify-content-centre">
-            <div class="col-md-4">
-                <div class="card card-default">
-                    <div class="card-header">Add new task</div>
+    <div class="row">
+        <div class="col-md-8 offset-2">
+            <div class="tile">
+                <div class="tile-body">
+                    <form action="{{route('task.save')}}" method="post">
+                        {{csrf_field()}}
 
-                    <div class="card-body">
-                        <form action="{{route('task.save')}}" method="post">
-                            {{csrf_field()}}
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <input type="text" name="description" id="description" class="form-control">
+                            <span class="error-text">{{$errors->first('description')}}</span>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="description">Description</label>
-                                <input type="text" name="description" id="description" class="form-control">
-                                <span class="error-text">{{$errors->first('description')}}</span>
-                            </div>
-
-                            <button class="btn btn-success">Save</button>
-                        </form>
-                    </div>
+                        <button class="btn btn-success">Save</button>
+                    </form>
                 </div>
             </div>
         </div>
